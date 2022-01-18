6cf257ec6015f19efd4ee44810d2c48f2fac473a
Samir Jain <samir.jain@contoso.com>
2022-01-18 17:25:34 +0530

    appname

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully 2022."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
