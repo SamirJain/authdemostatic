f39d87b566f12401a02bf6cf9aae36ce2145dace
Samir Jain <samir.jain@contoso.com>
2022-01-18 20:13:54 +0530

    done

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP trigger function processed a request modified one.');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
